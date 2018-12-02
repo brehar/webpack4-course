d3e977a15e7f45fce8bb4572fba90ad6e66405a8
Brett Hartman <brett.hartman@mac.com>
2018-12-02 09:24:45 -0800

    Finish implementation of hot module replacement on express server

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const webpack = require('webpack');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
 	entry: {
@@ -45,19 +46,7 @@ module.exports = {
 				test: /\.html$/,
 				use: [
 					{
-						loader: 'file-loader',
-						options: {
-							name: '[name].html'
-						}
-					},
-					{
-						loader: 'extract-loader'
-					},
-					{
-						loader: 'html-loader',
-						options: {
-							attrs: ['img:src']
-						}
+						loader: 'html-loader'
 					}
 				]
 			},
@@ -74,5 +63,10 @@ module.exports = {
 			}
 		]
 	},
-	plugins: [new webpack.HotModuleReplacementPlugin()]
+	plugins: [
+		new webpack.HotModuleReplacementPlugin(),
+		new HTMLWebpackPlugin({
+			template: './src/index.html'
+		})
+	]
 };
